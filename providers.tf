bf8d957fb43de83727b0a667751d06255ade1090
raksit31667 <raksit.m@ku.th>
2023-03-06 16:17:38 +0700

    :bricks: define Azure Terraform backend configuration

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "example-platform-tfstate"
+    storage_account_name = "exampleplatformtfstate"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 }
