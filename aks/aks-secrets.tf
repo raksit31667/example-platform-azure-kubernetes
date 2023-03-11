7da29d0044b63ffe5cec9b1c9ac9c3db0ba1af87
raksit31667 <raksit.m@ku.th>
2023-03-11 16:32:31 +0700

    :bricks: store AKS secrets inside KeyVault

---
+++ aks/aks-secrets.tf
@@ -0,0 +1,67 @@
+resource "azurerm_key_vault_secret" "aks_client_key" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-client-key"
+  value        = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_client_certificate" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-client-certificate"
+  value        = azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_cluster_ca_certificate" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-cluster-ca-certificate"
+  value        = azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_cluster_username" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-cluster-username"
+  value        = azurerm_kubernetes_cluster.aks.kube_admin_config.0.username
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_admin_client_key" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-admin-client-key"
+  value        = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key : ""
+  key_vault_id = var.key_vault_id
+}
+resource "azurerm_key_vault_secret" "aks_admin_client_certificate" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-admin-client-certificate"
+  value        = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate : ""
+  key_vault_id = var.key_vault_id
+}
+resource "azurerm_key_vault_secret" "aks_admin_cluster_ca_certificate" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-admin-cluster-ca-certificate"
+  value        = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate : ""
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_admin_username" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-admin-username"
+  value        = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config.0.username : ""
+  key_vault_id = var.key_vault_id
+}
+resource "azurerm_key_vault_secret" "aks_admin_password" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-admin-password"
+  value        = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config.0.password : ""
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_service_principal_id" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-service-principal-id"
+  value        = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_secret" "aks_kubelet_identity_client_id" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-kubelet-identity-client-id"
+  value        = azurerm_kubernetes_cluster.aks.kubelet_identity[0].client_id
+  key_vault_id = var.key_vault_id
+}
+resource "azurerm_key_vault_secret" "aks_host" {
+  name         = "${azurerm_kubernetes_cluster.aks.name}-host"
+  value        = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+  key_vault_id = var.key_vault_id
+}
