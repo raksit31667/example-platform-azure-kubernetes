3a1b617157d5b14b252344b321902c0ed6f74fd4
raksit31667 <raksit.m@ku.th>
2023-03-11 16:37:13 +0700

    :bricks: fix invalid index problem during Terraform plan on AKS

--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,21 +1,21 @@
 output "aks_principal_id" {
-  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+  value = length(azurerm_kubernetes_cluster.aks.identity) > 0 ? azurerm_kubernetes_cluster.aks.identity[0].principal_id : ""
 }
 
 output "kube_admin_config_host" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].host : ""
 }
 
 output "kube_admin_config_client_cert" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate : ""
 }
 
 output "kube_admin_config_client_key" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key : ""
 }
 
 output "kube_admin_config_ca_cert" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate
+  value = length(azurerm_kubernetes_cluster.aks.kube_admin_config) > 0 ? azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate : ""
 }
 
 output "kube_config" {
