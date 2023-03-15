b6be464ecb135bc29f1ada832883ca2f499ec7a5
raksit31667 <raksit.m@ku.th>
2023-03-15 23:37:09 +0700

    :fire: remove AKS secrets module

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -21,7 +21,6 @@ resource "azurerm_key_vault" "key_vault" {
   resource_group_name         = var.resource_group_name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
