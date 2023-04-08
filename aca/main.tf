3f586b7b92ad1535ef58559cab29039426019c1f
raksit31667 <raksit.m@ku.th>
2023-04-08 15:10:25 +0700

    :bricks: change ACA log analytics SKU to PerGB2018
    Pricing tier doesn't match the subscription's billing model

--- aca/main.tf
+++ aca/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   name                = "exampleplatformloganalytics"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "Free"
+  sku                 = "PerGB2018"
   retention_in_days   = 7
 }
 
