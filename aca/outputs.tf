01b01df3192b10b4ca50665bfb351907cfe74e85
raksit31667 <raksit.m@ku.th>
2023-06-03 17:30:57 +0700

    :bricks: remove unused ACA outputs

--- aca/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aca_environment_id" {
-  value = azurerm_container_app_environment.aca_environment[*].id
-}
-
-output "aca_user_identity_id" {
-  value = azurerm_user_assigned_identity.aca_user_identity[*].id
-}
